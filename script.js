55fab422139fa62ef9cea4eece812b8b21c63667
Jordan Moureau <jordanmoureau@Jordans-iMac.local>
2023-05-20 11:27:35 -0700

    responsive fixes and image updates

--- script.js
+++ script.js
@@ -130,13 +130,15 @@ function functJavaScript() {
 
  const form = document.getElementById('form');
 const result = document.getElementById('result');
+const message = document.getElementById('message');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
-  result.innerHTML = "Sending your message..."
+  message.innerHTML = "<p>Sending your message...</p>";
+  document.getElementById("submit").style.display = "none";
 
     fetch('https://api.web3forms.com/submit', {
             method: 'POST',
@@ -149,7 +151,9 @@ form.addEventListener('submit', function(e) {
         .then(async (response) => {
             let json = await response.json();
             if (response.status == 200) {
-                result.innerHTML = json.message;
+                result.innerHTML = "<h3>Message Sent!</h3><p>I'll be in touch.</p>";
+                form.style.display = "none";
+                message.style.display = "none";
             } else {
                 console.log(response);
                 result.innerHTML = json.message;
@@ -165,6 +169,254 @@ form.addEventListener('submit', function(e) {
             form.reset();
             setTimeout(() => {
                 result.style.display = "none";
-            }, 3000);
+                form.style.display = "block";
+                document.getElementById("submit").style.display = "block";
+            }, 6000);
         });
 });
+
+//hover state for portfolio
+
+function bigOneMask() {
+  document.getElementById("bigOne").style.backgroundColor = "#fff";
+  document.getElementById("bigOne").style.backgroundImage = "none";
+  document.getElementById("bigOne").innerHTML = "The Mission is Love.";
+}
+
+function bigOneClick (){
+  window.open("https://www.loveandflowers.com");
+}
+
+function bigOneOut() {
+    document.getElementById("bigOne").style.backgroundColor = "white";
+  document.getElementById("bigOne").innerHTML = "";
+  document.getElementById("bigOne").style.backgroundImage = "url(images/loveandflowers.png)";
+}
+
+function bigTwoMask() {
+  document.getElementById("bigTwo").style.backgroundColor = "#fff";
+  document.getElementById("bigTwo").style.backgroundImage = "none";
+  document.getElementById("bigTwo").innerHTML = "Poems and Grids.";
+}
+
+function bigTwoClick (){
+  window.open("https://www.emilywinter.com");
+}
+
+function bigTwoOut() {
+    document.getElementById("bigTwo").style.backgroundColor = "white";
+  document.getElementById("bigTwo").innerHTML = "";
+  document.getElementById("bigTwo").style.backgroundImage = "url(images/emilywinter.png)";
+}
+
+function bigThreeClick (){
+  window.open("https://www.emilywinterpoetry.com");
+}
+
+function bigThreeMask() {
+  document.getElementById("bigThree").style.backgroundColor = "#fff";
+  document.getElementById("bigThree").style.backgroundImage = "none";
+  document.getElementById("bigThree").innerHTML = "That's Ms Developer to You.";
+}
+
+function bigThreeOut() {
+    document.getElementById("bigThree").style.backgroundColor = "white";
+  document.getElementById("bigThree").innerHTML = "";
+  document.getElementById("bigThree").style.backgroundImage = "url(images/blog.png)";
+}
+
+function bigFourMask() {
+  document.getElementById("bigFour").style.backgroundColor = "#fff";
+  document.getElementById("bigFour").style.backgroundImage = "none";
+  document.getElementById("bigFour").innerHTML = "A text heavy blog concept.";
+}
+
+function bigFourClick (){
+  window.open("https://www.frontendblog.com");
+}
+
+function bigFourOut() {
+    document.getElementById("bigFour").style.backgroundColor = "white";
+  document.getElementById("bigFour").innerHTML = "";
+  document.getElementById("bigFour").style.backgroundImage = "url(images/wtwc.png)";
+}
+
+function bigFiveMask() {
+  document.getElementById("bigFive").style.backgroundColor = "#fff";
+  document.getElementById("bigFive").style.backgroundImage = "none";
+  document.getElementById("bigFive").innerHTML = "For Keeping track of my Life.";
+}
+
+function bigFiveClick (){
+  window.open("https://www.wtwc.com");
+}
+
+function bigFiveOut() {
+  document.getElementById("bigFive").innerHTML = "";
+  document.getElementById("bigFive").style.backgroundImage = "url(images/inayearsite-01.png)";
+}
+
+function littleOneMask() {
+  document.getElementById("littleOne").style.gridArea = "1 / 3 / span2 / span2";
+  document.getElementById("littleTwo").style.display = "none";
+  document.getElementById("littleThree").style.display = "none";
+  document.getElementById("littleFour").style.display = "none";
+  document.getElementById("littleOne").style.backgroundSize= "contain";
+}
+
+function littleOneOut() {
+  document.getElementById("littleOne").style.gridArea = "1 / 3 / 1 / 3";
+document.getElementById("littleOne").style.backgroundImage = "url(images/INAYEAR.png)";
+document.getElementById("littleTwo").style.display = "block";
+document.getElementById("littleThree").style.display = "block";
+document.getElementById("littleFour").style.display = "block";
+document.getElementById("littleOne").style.backgroundSize= "330px";
+}
+
+function littleTwoMask() {
+  document.getElementById("littleTwo").style.gridArea = "1 / 3 / span2 / span2";
+  document.getElementById("littleOne").style.display = "none";
+  document.getElementById("littleThree").style.display = "none";
+  document.getElementById("littleFour").style.display = "none";
+}
+
+function littleTwoOut() {
+  document.getElementById("littleTwo").style.gridArea = "1 / 4 / 1 / 4";
+document.getElementById("littleOne").style.display = "block";
+document.getElementById("littleThree").style.display = "block";
+document.getElementById("littleFour").style.display = "block";
+}
+
+function littleThreeMask() {
+  document.getElementById("littleThree").style.gridArea = "1 / 3 / span2 / span2";
+  document.getElementById("littleOne").style.display = "none";
+  document.getElementById("littleTwo").style.display = "none";
+  document.getElementById("littleFour").style.display = "none";
+}
+
+function littleThreeOut() {
+  document.getElementById("littleThree").style.gridArea = "2 / 3 / 2 / 3";
+document.getElementById("littleOne").style.display = "block";
+document.getElementById("littleTwo").style.display = "block";
+document.getElementById("littleFour").style.display = "block";
+}
+
+function littleFourMask() {
+  document.getElementById("littleFour").style.gridArea = "1 / 3 / span2 / span2";
+  document.getElementById("littleOne").style.display = "none";
+  document.getElementById("littleTwo").style.display = "none";
+  document.getElementById("littleThree").style.display = "none";
+  document.getElementById("littleFour").style.backgroundSize= "cover";
+}
+
+function littleFourOut() {
+  document.getElementById("littleFour").style.gridArea = "2 / 4 / 2 / 4";
+document.getElementById("littleOne").style.display = "block";
+document.getElementById("littleTwo").style.display = "block";
+document.getElementById("littleThree").style.display = "block";
+document.getElementById("littleFour").style.backgroundSize= "320px";
+}
+
+function littleNineMask() {
+  document.getElementById("littleNine").style.gridArea = "1 / 1 / span2 / 3";
+  document.getElementById("littleTen").style.display = "none";
+  document.getElementById("littleNine").style.backgroundSize= "cover";
+  document.getElementById("littleNine").style.backgroundPosition= "center";
+  document.getElementById("bigFive").style.display = "none";
+}
+
+function littleNineOut() {
+  document.getElementById("littleNine").style.gridArea = "1 / 2 / 1 / 2";
+document.getElementById("littleTen").style.display = "block";
+document.getElementById("littleNine").style.backgroundSize= "440px";
+document.getElementById("bigFive").style.display = "block";
+
+}
+
+function littleTenMask() {
+  document.getElementById("littleTen").style.gridArea = "1 / 1 / span2 / span 2";
+  document.getElementById("littleNine").style.display = "none";
+  document.getElementById("bigFive").style.display = "none";
+}
+
+function littleTenOut() {
+  document.getElementById("littleTen").style.gridArea = "2 / 2 / 2 / 2";
+document.getElementById("littleNine").style.display = "block";
+document.getElementById("bigFive").style.display = "block";
+
+}
+
+//begin JS for infinte slide for mobile portfolio display
+let sliderWrap = document.querySelector('.mobile-portfolio-wrap');
+let slider = document.querySelector('.mobile-portfolio');
+let clonesWidth;
+let sliderWidth;
+let clones = [];
+let disableScroll = false;
+let scrollPos;
+
+let items = [...document.querySelectorAll('.item')];
+let images = [...document.querySelectorAll('.img-div')];
+
+images.forEach((images, idx) => {
+    images.style.backgroundImage = `url(./images/${idx+1}.png)`
+})
+
+items.forEach(item => {
+    let clone = item.cloneNode(true);
+    clone.classList.add('clone');
+    slider.appendChild(clone);
+    clones.push(clone);
+})
+
+function getClonesWidth(){
+    let width = 0;
+    clones.forEach(clone => {
+        width += clone.offsetWidth;
+    })
+
+    return width;
+}
+
+function getScrollPos(){
+    return window.scrollY;
+}
+
+//takes a pixel position and sets window to scroll
+
+function scrollUpdate(){
+    if(window.innerWidth > 2000){
+      scrollPos = getScrollPos();
+      if(clonesWidth + scrollPos >= sliderWidth){
+          window.scrollTo({top:1});
+      } else if (scrollPos <= 0) {
+          window.scrollTo({top: sliderWidth - clonesWidth - 1})
+      }
+  
+      slider.style.transform = `translateX(${-window.scrollY}px)`
+  
+      requestAnimationFrame(scrollUpdate)
+    } else {
+      sliderWrap.style.overflow = 'scroll';
+    }
+  }
+
+function onLoad(){
+    calculateDimensions()
+    document.body.style.height = `${sliderWidth}px`
+    window.scrollTo({top:1});
+    scrollUpdate();
+}
+
+function calculateDimensions(){
+    sliderWidth = slider.getBoundingClientRect().width;
+    clonesWidth = getClonesWidth();
+}
+
+onLoad()
+
+const heroAction = setTimeout(heroFlip, 9000);
+
+function heroFlip(){
+  document.getElementById("hero").style.backgroundImage = "url(images/hero.png)";
+}
